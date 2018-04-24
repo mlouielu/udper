c6097d74a1282298e6649d582450650e53be343f
Louie Lu <git@louie.lu>
2018-04-24 16:58:20 +0800

    Let them connect

--- udper/client.py
+++ udper/client.py
@@ -1,3 +1,5 @@
+import pickle
+import time
 import socket
 
 
@@ -8,14 +10,31 @@ def create_udp(dscp=0x0):
     return sock
 
 
+def create_udp_recv(ip, port, timeout=3):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind((ip, port))
+    sock.settimeout(timeout)
+    return sock
+
+
 def sendto(sock, host, port, data):
     sock.sendto(data, (host, port))
 
 
 if __name__ == '__main__':
-    sock = create_udp()
-    DUMMY_1 = b'\x00' * 1024
-    DUMMY_2 = b'\xff' * 1024
+    recv = create_udp_recv('0.0.0.0', 9000)
+    sock_1 = create_udp()
+    sock_2 = create_udp(0xE0)
+    DUMMY_1 = b'\x00' * 160
+    DUMMY_2 = b'\x10' * 160
 
-    sendto(sock, 'localhost', 6600, DUMMY_1)
-    sendto(sock, 'localhost', 6600, DUMMY_2)
+    for t in range(10):
+        for pk in range(60):
+            for _ in range(50):
+                sendto(sock_2, 'localhost', 6600, DUMMY_2)
+            for _ in range(50):
+                sendto(sock_1, 'localhost', 6600, DUMMY_1)
+        time.sleep(4)
+        print('RECV')
+        data, addr = recv.recvfrom(65535)
+        print(pickle.loads(data))
--- udper/serv.py
+++ udper/serv.py
@@ -1,23 +1,35 @@
+import pickle
 import socket
 from collections import Counter
 
 
 BUFFER_SIZE = 65535
+BACKTO = ('192.168.231.234', 9000)
 
 
-def create_udp(ip, port):
+def create_udp(ip, port, timeout=2):
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.bind((ip, port))
+    sock.settimeout(timeout)
     return sock
 
 
 def receive_and_count(sock):
+    udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     c = Counter()
     while True:
-        data, addr = sock.recvfrom(BUFFER_SIZE)
-        c[hash(data)] += 1
+        try:
+            data, addr = sock.recvfrom(BUFFER_SIZE)
+        except socket.timeout:
+            if c:
+                print('RESULT')
+                print(c)
 
-        print(c)
+                # Send back
+                udp.sendto(pickle.dumps(c), BACKTO)
+            c = Counter()
+        else:
+            c[data[0]] += 1
 
 
 if __name__ == '__main__':
